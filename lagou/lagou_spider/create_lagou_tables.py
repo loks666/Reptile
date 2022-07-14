e6dfee39f457e5e52617e1ad03b410e8ca412733
Jax <284190056@qq.com>
2022-07-14 22:28:24 +0800

    add covert_data.py

--- lagou/lagou_spider/create_lagou_tables.py
+++ lagou/lagou_spider/create_lagou_tables.py
@@ -16,43 +16,43 @@ class Lagoutables(Base):
     # 表名称
     __tablename__ = 'lagou_data'
     # id,设置为主键和自动增长
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, comment='ID号')
     # 岗位ID,非空字段
-    positionID = Column(Integer, nullable=True)
+    positionID = Column(Integer, nullable=True, comment='岗位ID')
     # 经度
-    longitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False, comment='经度')
     # 纬度
-    latitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=False, comment='纬度')
     # 岗位名称
-    positionName = Column(String(length=50), nullable=False)
+    positionName = Column(String(length=50), nullable=False, comment='岗位名称')
     # 工作年限
-    workYear = Column(String(length=20), nullable=False)
+    workYear = Column(String(length=20), nullable=False, comment='工作年限')
     # 学历
-    education = Column(String(length=20), nullable=False)
+    education = Column(String(length=20), nullable=False, comment='学历')
     # 岗位性质
-    jobNature = Column(String(length=20), nullable=True)
+    jobNature = Column(String(length=20), nullable=True, comment='岗位性质')
     # 公司类型
-    financeStage = Column(String(length=30), nullable=True)
+    financeStage = Column(String(length=30), nullable=True, comment='公司类型')
     # 公司规模
-    companySize = Column(String(length=30), nullable=True)
+    companySize = Column(String(length=30), nullable=True, comment='公司规模')
     # 业务方向
-    industryField = Column(String(length=30), nullable=True)
+    industryField = Column(String(length=30), nullable=True, comment='业务方向')
     # 所在城市
-    city = Column(String(length=10), nullable=False)
+    city = Column(String(length=10), nullable=False, comment='所在城市')
     # 岗位标签
-    positionAdvantage = Column(String(length=200), nullable=True)
+    positionAdvantage = Column(String(length=200), nullable=True, comment='岗位标签')
     # 公司简称
-    companyShortName = Column(String(length=50), nullable=True)
+    companyShortName = Column(String(length=200), nullable=True, comment='公司简称')
     # 公司全称
-    companyFullName = Column(String(length=200), nullable=True)
+    companyFullName = Column(String(length=200), nullable=True, comment='公司全称')
     # 公司所在区
-    district = Column(String(length=20), nullable=True)
+    district = Column(String(length=20), nullable=True, comment='公司所在区')
     # 公司福利标签
-    companyLabelList = Column(String(length=200), nullable=True)
+    companyLabelList = Column(String(length=200), nullable=True, comment='公司福利标签')
     # 工资
-    salary = Column(String(length=20), nullable=False)
+    salary = Column(String(length=20), nullable=False, comment='工资')
     # 抓取日期
-    crawl_date = Column(String(length=20), nullable=False)
+    crawl_date = Column(String(length=20), nullable=False, comment='抓取日期')
 
 
 if __name__ == '__main__':
--- lagou/lagou_spider/handle_crawl_lagou.py
+++ lagou/lagou_spider/handle_crawl_lagou.py
@@ -1,40 +1,49 @@
 import json
-import re
-import time
 import requests
 import multiprocessing
 # from lagou_spider.handle_insert_data import lagou_mysql
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import re
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 from lagou.lagou_spider.handle_insert_data import lagou_mysql
+
+s = Service(executable_path=r'H:\Project\Python\Reptile\51job\chromedriver.exe')
+browser = webdriver.Chrome(service=s)
+browser.set_window_position(3841, 0)
+browser.maximize_window()
+wait = WebDriverWait(browser, 10)
 
 
 class HandleLaGou(object):
     def __init__(self):
         # 使用session保存cookies信息
-        self.lagou_session = requests.session()
+        self.lagou_session = browser.get_cookies()
         self.header = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
         }
-        self.city_list = ""
-
-    # 获取全国所有城市列表的方法
-    def handle_city(self):
-        city_search = re.compile(r'www\.lagou\.com\/.*\/">(.*?)</a>')
-        city_url = "https://www.lagou.com/jobs/allCity.html"
-        city_result = self.handle_request(method="GET", url=city_url)
-        # 使用正则表达式获取城市列表
-        self.city_list = set(city_search.findall(city_result))
-        self.lagou_session.cookies.clear()
+        # 读取json文件内容,返回字典格式
+        with open('citys.json', 'r', encoding='utf8') as fp1:
+            json_data = json.load(fp1)
+        self.city_list = set(json_data['city'])
+        # self.lagou_session.cookies.clear()
+        # handle_city()
 
     def handle_city_job(self, city):
         first_request_url = "https://www.lagou.com/jobs/list_python?city=%s&cl=false&fromSearch=true&labelWords=&suginput=" % city
-        first_response = self.handle_request(method="GET", url=first_request_url)
+        browser.get(first_request_url)
+        first_response = browser.page_source
+        # first_response = self.handle_request(method="GET", url=first_request_url)
         total_page_search = re.compile(r'class="span\stotalNum">(\d+)</span>')
         try:
             total_page = total_page_search.search(first_response).group(1)
             print(city, total_page)
         # 由于没有岗位信息造成的exception
-        except:
-            return
+        except Exception as e:
+            return e
         else:
             for i in range(1, int(total_page) + 1):
                 data = {
@@ -44,9 +53,11 @@ class HandleLaGou(object):
                 page_url = "https://www.lagou.com/jobs/positionAjax.json?city=%s&needAddtionalResult=false" % city
                 referer_url = "https://www.lagou.com/jobs/list_python?city=%s&cl=false&fromSearch=true&labelWords=&suginput=" % city
                 # referer的URL需要进行encode
-                self.header['Referer'] = referer_url.encode()
-                response = self.handle_request(method="POST", url=page_url, data=data, info=city)
-                lagou_data = json.loads(response)
+                # self.header['Referer'] = referer_url.encode()
+                browser.get(first_request_url)
+                response = browser.page_source
+                # response = self.handle_request(method="POST", url=page_url, data=data, info=city)
+                lagou_data = json.loads(response.text())
                 job_list = lagou_data['content']['positionResult']['result']
                 for job in job_list:
                     lagou_mysql.insert_item(job)
@@ -54,7 +65,8 @@ class HandleLaGou(object):
     def handle_request(self, method, url, data=None, info=None):
         while True:
             # 加入阿布云的动态代理
-            proxyinfo = "http://%s:%s@%s:%s" % ('H1V32R6470A7G90D', 'CD217C660A9143C3', 'http-dyn.abuyun.com', '9020')
+            proxyinfo = "http://%s:%s@%s:%s" % (
+                '864839856801861632', 'cTQvA3Ib', 'http-short.xiaoxiangdaili.com', '10010')
             proxy = {
                 "http": proxyinfo,
                 "https": proxyinfo
@@ -88,14 +100,29 @@ class HandleLaGou(object):
             return response.text
 
 
+# 点击
+def click(selector):
+    button = wait.until(EC.element_to_be_clickable((By.XPATH, selector)))
+    button.click()
+
+
+# css文本输入
+def input_keys(selector, keys):
+    inputs = wait.until(EC.presence_of_element_located((By.XPATH, selector)))
+    inputs.clear()
+    inputs.send_keys(keys)
+
+
+def login():
+    browser.get('https://www.lagou.com/')
+    browser.refresh()
+    click('/html/body/div[10]/div[1]/div[2]/div[2]/button[4]')
+    click('//*[@id="lg_tbar"]/div[1]/div[2]/ul/li[1]/a')
+    input("登陆完成按回车")
+
+
 if __name__ == '__main__':
+    login()
     lagou = HandleLaGou()
-    # 所有城市的方法
-    lagou.handle_city()
-    print(lagou.city_list)
-    # 引入多进程加速抓取
-    pool = multiprocessing.Pool(2)
     for city in lagou.city_list:
-        pool.apply_async(lagou.handle_city_job, args=(city,))
-    pool.close()
-    pool.join()
+        lagou.handle_city_job(city)
